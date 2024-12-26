74d3ac86d3b03da0be52176fe0658aa2c74f15dc
Sarah <sarahdejesuslima07@gmail.com>
2024-12-26 12:16:25 -0300

    Adicionado download somente do audio

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ from pytubefix.cli import on_progress
 url = "https://youtu.be/1vs1yCkDskg?si=a4MiAVgVXbzISrdV"
 
 yt = YouTube(url, on_progress_callback=on_progress)
-print(yt.title)
 
 ys = yt.streams.get_highest_resolution()
+ys.download()
+
+yt = YouTube(url, on_progress_callback=on_progress)
+print(yt.title)
+
+ys = yt.streams.get_audio_only()
 ys.download()
