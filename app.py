9c4f61e578217a5f2a771af5853b3895e83f7b2b
Sarah <sarahdejesuslima07@gmail.com>
2024-12-26 09:17:43 -0300

    Projeto Youtube Downloader

---
+++ app.py
@@ -0,0 +1,10 @@
+from pytubefix import *
+from pytubefix.cli import on_progress
+
+url = "https://youtu.be/1vs1yCkDskg?si=a4MiAVgVXbzISrdV"
+
+yt = YouTube(url, on_progress_callback=on_progress)
+print(yt.title)
+
+ys = yt.streams.get_highest_resolution()
+ys.download()
