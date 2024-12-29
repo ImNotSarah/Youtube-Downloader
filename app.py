d0d7e29f964a93dac30fa91b0731dbc8143beb09
Sarah <sarahdejesuslima07@gmail.com>
2024-12-28 22:02:32 -0300

    Adicionado opções de download

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from pytubefix import *
 from pytubefix.cli import on_progress
 
-url = "https://youtu.be/1vs1yCkDskg?si=a4MiAVgVXbzISrdV"
+url = "https://www.youtube.com/watch?v=3AxBl2XDXuw"
 
 yt = YouTube(url, on_progress_callback=on_progress)
 
-ys = yt.streams.get_highest_resolution()
-ys.download()
+streams = yt.streams
 
-yt = YouTube(url, on_progress_callback=on_progress)
-print(yt.title)
+for stream in streams:
+    if stream.type == "video":
+        print(f"{stream.type} | {stream.resolution} | {stream.itag}")
+    else:
+        print(f"{stream.type} | {stream.abr} | {stream.itag}")
 
-ys = yt.streams.get_audio_only()
-ys.download()
+
+option_chosen = input("write the tag number of the resolution you want: ")
+
+donwload = streams.get_by_itag(option_chosen)
+
+donwload.download()
